c5ab02083b6b7c2be5cc365c0eabe3170cf8659f
Leila <nb.leila10@gmail.com>
2020-01-09 21:14:28 -0600

    Count Triplets - Very efficient solution

--- javascript/dictsAndHashmaps/countTriplets.js
+++ javascript/dictsAndHashmaps/countTriplets.js
@@ -15,35 +15,38 @@ r: an integer, the common ratio
 */
 
 function countTriplets(arr, r) {
-  const arrDict = {};
-  for (let i = arr.length - 1; i >= 0; i--) {
-    if (arr[i] in arrDict) arrDict[arr[i]].push(i);
-    else arrDict[arr[i]] = [i];
-  }
-  console.log(arrDict);
+  // stores number of arrays with two elements that can be formed if we find the key
+  const potential_two = {};
+  // stores number of arrays with three elements that can be formed if we find the key
+  const potential_three = {};
   var count = 0;
-
-  for (let i = 0; i < arr.length - 2; i++) {
-    // how many arr[i]*r are there after i
-    const second = arr[i] * r;
-    console.log("second: ", second);
-    if (!(second in arrDict)) continue;
-    const secondIndex = arrDict[second].filter(index => index > i);
-    // how many second*r are there after i
-    const third = second * r;
-    console.log("third: ", third);
-    if (!(third in arrDict)) continue;
-    secondIndex.forEach(idx => {
-      count += arrDict[third].filter(index => index > idx).length;
-    });
+  for (num of arr) {
+    // num completes the three arrays given we have already found num/(r^2) and num/r
+    if (num in potential_three) count += potential_three[num];
+    // For any element of array we can form three element tuples if we find num*r given num/r is already found.
+    // Also num forms the second element.
+    if (!(num in potential_two)) potential_two[num] = 0;
+    if (num * r in potential_three) potential_three[num * r] += potential_two[num];
+    else potential_three[num * r] = potential_two[num];
+    // For any element of array we can form two element tuples if we find num*r.
+    // Also num forms the first element.
+    if (num * r in potential_two) potential_two[num * r] += 1;
+    else potential_two[num * r] = 1;
   }
-
   return count;
 }
 
-// const arr = [1, 4, 16, 64];
-// console.log(countTriplets(arr, 4));
-const arr = [1, 2, 2, 4];
-console.log(countTriplets(arr, 2));
-// const arr = [1, 1, 1];
+const arr1 = [1, 4, 16, 64];
+console.log(countTriplets(arr1, 4));
+// const arr2 = [1, 2, 2, 4];
+// console.log(countTriplets(arr2, 2));
+// const arr3 = [3, 2, 3, 4, 3];
+// console.log(countTriplets(arr3, 1));
+// const arr4 = [1, 3, 9, 9, 27, 81];
+// console.log(countTriplets(arr4, 3));
+// const arr = [1, 2, 1, 2, 4];
+// console.log(countTriplets(arr, 2));
+// const str =
+//   "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1";
+// const arr = str.split(" ");
 // console.log(countTriplets(arr, 1));
