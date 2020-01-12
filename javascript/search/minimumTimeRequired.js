5f3223311d17c7cad05ecddb8960c77236aeddf1
Leila <nb.leila10@gmail.com>
2020-01-12 17:19:20 -0600

    Used binary search for optimal solution

--- javascript/search/minimumTimeRequired.js
+++ javascript/search/minimumTimeRequired.js
@@ -26,35 +26,50 @@ goal: an integer, the number of items required to complete the order
 */
 
 function minTime(machines, goal) {
-  var count = 0;
-  // build dict with how many machines take d days
-  var days = {};
-  for (day of machines) {
-    if (day in days) days[day]++;
-    else days[day] = 1;
-  }
-  var day = 1;
-  var keys = Object.keys(days);
-  while (count <= goal) {
-    for (num of keys) {
-      if (day % parseInt(num) === 0) {
-        count += days[num];
-        if (count >= goal) return day;
-      }
-      console.log(day % parseInt(num), count, day);
+  // When we're performing a search we need to start by defining some bounds
+  // (we need to search between two bounds otherwise how do we know where to start/stop)
+
+  // Well let's start with the upper bound.
+  // How many days would it take if ALL machines were equally as slow as the slowest machine?
+  //  That means the worst case is goal / machines * slowestMachine
+  const min = Math.min(...machines);
+  let minDay = Math.ceil(goal / machines.length) * min;
+
+  // Same thing to find the lower bound.
+  // How many days would it take if ALL machines were equally as fast as the fastest machine?
+  // Our lower bound is then goal / machines * fastestMachine
+  const max = Math.max(...machines);
+  let maxDay = Math.ceil(goal / machines.length) * max;
+
+  const getSum = (arr, d) => arr.reduce((total, machine) => total + Math.floor(d / machine), 0);
+
+  while (minDay < maxDay) {
+    // Now that we have our lower and upper bounds, we can finally start to search.
+    // With binary search we always start at the middle of our bounds. (lowerBound + upperBound) / 2
+    const day = Math.floor((maxDay + minDay) / 2);
+
+    // Next we'll try to calculate how many items would be produced after this many days.
+    const sum = getSum(machines, day);
+
+    // Now we check if we've met our goal or not.
+    if (sum >= goal) {
+      // If we produce more than our goal, then the half way point that we chose is too high
+      maxDay = day;
+    } else {
+      // If we produced less then the half way point was too low.
+      minDay = day + 1;
     }
-    day++;
   }
-  return day;
+  return minDay;
 }
 
-// const machines = [2, 3, 2];
-// console.log(minTime(machines, 10));
+const machines = [2, 3, 2];
+console.log(minTime(machines, 10));
 // const machines = [2, 3];
 // console.log(minTime(machines, 5));
 // const machines = [1, 3, 4];
 // console.log(minTime(machines, 10));
 // const machines = [4, 5, 6];
 // console.log(minTime(machines, 12));
-const machines = [2, 1];
-console.log(minTime(machines, 1));
+// const machines = [2, 1];
+// console.log(minTime(machines, 1));
