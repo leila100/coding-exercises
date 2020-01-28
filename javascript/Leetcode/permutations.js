5e9b3f0ce55a06ed6fc7148ffbd6ae350c89c176
Leila <nb.leila10@gmail.com>
2020-01-27 19:46:45 -0600

    Permutations - recursive version

--- javascript/Leetcode/permutations.js
+++ javascript/Leetcode/permutations.js
@@ -33,4 +33,34 @@ var permute = function(nums) {
   return solution;
 };
 
-console.log(permute([1, 2, 3]));
+var permuteRecursive = function(nums) {
+  let output = [];
+
+  recurFill([], nums);
+
+  function recurFill(permutation, nums_remaining) {
+    // Base case, all numbers added
+    if (nums_remaining.length == 0) {
+      output.push(permutation.slice());
+      return;
+    }
+
+    for (let i = 0; i < nums_remaining.length; i++) {
+      // Add the current number to the permutation
+      permutation.push(nums_remaining[0]);
+      // Take the remaining numbers and remove the first element
+      let removed = nums_remaining.shift();
+      // Call the recursive function with the current permutation
+      recurFill(permutation, nums_remaining);
+      // Pop the number off the permutation again
+      permutation.pop();
+      // Add the number from the remaining numbers for permutations back into the array
+      nums_remaining.push(removed);
+    }
+  }
+
+  return output;
+};
+
+// console.log(permute([1, 2]));
+console.log(permuteRecursive([1, 2, 3]));
