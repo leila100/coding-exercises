929eead7181a3a548e6714ba189234dddb43fbfa
Leila <nb.leila10@gmail.com>
2020-01-31 16:32:05 -0600

    Add more efficient way to find the merge node of two linked lists

--- javascript/mergeNode.js
+++ javascript/mergeNode.js
@@ -13,15 +13,54 @@ It is guaranteed that the two head Nodes will be different, and neither will be 
     }
 */
 
+// not efficient O(m * n)
+// function findMergeNode(headA, headB) {
+//   let currentA = headA;
+//   while (currentA) {
+//     let currentB = headB;
+//     while (currentB) {
+//       if (currentB === currentA) return currentB.data;
+//       currentB = currentB.next;
+//     }
+//     currentA = currentA.next;
+//   }
+//   return null;
+// }
+
+function listLength(head) {
+  let length = 0;
+  let current = head;
+  while (current) {
+    length++;
+    current = current.next;
+  }
+  return length;
+}
+
+// more efficient O(m+n)
 function findMergeNode(headA, headB) {
+  const lenA = listLength(headA);
+  const lenB = listLength(headB);
+  let diff = 0;
+
+  // get the difference of length
+  // traverse the largest list until both are same length
+  if (lenA > lenB) {
+    diff = lenA - lenB;
+    for (let i = 0; i < diff; i++) headA = headA.next;
+  } else {
+    diff = lenB - lenA;
+    for (let i = 0; i < diff; i++) headB = headB.next;
+  }
+
   let currentA = headA;
+  let currentB = headB;
+
+  // traverse list until find connecting node
   while (currentA) {
-    let currentB = headB;
-    while (currentB) {
-      if (currentB === currentA) return currentB.data;
-      currentB = currentB.next;
-    }
+    if (currentA === currentB) return currentA.data;
     currentA = currentA.next;
+    currentB = currentB.next;
   }
   return null;
 }
