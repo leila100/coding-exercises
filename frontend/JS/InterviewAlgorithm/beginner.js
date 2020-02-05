d73a898aeff4d4909c0e2ff27054806826d155e9
Leila <nb.leila10@gmail.com>
2020-02-04 21:43:08 -0600

    solve question 15: Find the largest sum of any two elements

--- frontend/JS/InterviewAlgorithm/beginner.js
+++ frontend/JS/InterviewAlgorithm/beginner.js
@@ -260,10 +260,37 @@ function sumFinder(arr, sum) {
   }
   return false;
 }
-console.log(sumFinder([6, 4, 3, 2, 1, 7], 9));
-console.log(sumFinder([6, 4, 3, 2, 1, 7], 2));
+// console.log(sumFinder([6, 4, 3, 2, 1, 7], 9));
+// console.log(sumFinder([6, 4, 3, 2, 1, 7], 2));
 
-// Find the largest sum of any two elements?
+// 15- Find the largest sum of any two elements?
+function topSum(arr) {
+  // if (arr.length === 0) return 0;
+  // if (arr.length === 1) return arr[0]
+  // arr.sort((a, b) => a-b)
+  // return arr[0] + arr[1]
+
+  //better complexity
+  // Just find the two largest number and return sum of them
+  let max1 = arr[0];
+  let max2 = arr[1];
+  if (max1 < max2) {
+    max1 = arr[1];
+    max2 = arr[0];
+  }
+  for (let i = 2; i < arr.length; i++) {
+    if (max1 < arr[i]) {
+      max2 = max1;
+      max1 = arr[i];
+    } else if (arr[i] > max2) max2 = arr[i];
+  }
+  return max1 + max2;
+}
+
+console.log(topSum([6, 4, 3, 2, 1, 7]));
+console.log(topSum([3, 31, 7, 2, 10, 1, 0]));
+console.log(topSum([3, 0]));
+
 // Total number of zeros from 1 upto n?
 // Match substring of a sting?
 // Create all permutation of a string?
