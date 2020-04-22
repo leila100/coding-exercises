d4cf764d2cea988066f5c9e7910a7b5786ae408d
Leila <nb.leila10@gmail.com>
2020-04-21 19:14:24 -0500

    Answer challenge 3

--- frontend/JS/async/async.js
+++ frontend/JS/async/async.js
@@ -99,15 +99,17 @@ function delayedGreet() {
   setTimeout(() => console.log("Welcome"), 3000);
 }
 // Uncomment the following line to check your work!
-delayedGreet(); // should log (after 3 seconds): welcome
+// delayedGreet(); // should log (after 3 seconds): welcome
 
 /* CHALLENGE 3 */
+// Create a function helloGoodbye that console logs hello right away, and good bye after 2 seconds.
 
 function helloGoodbye() {
-  // ADD CODE HERE
+  console.log("hello");
+  setTimeout(() => console.log("good bye"), 2000);
 }
 // Uncomment the following line to check your work!
-// helloGoodbye(); // should log: hello // should also log (after 3 seconds): good bye
+helloGoodbye(); // should log: hello // should also log (after 2 seconds): good bye
 
 /* CHALLENGE 4 */
 
