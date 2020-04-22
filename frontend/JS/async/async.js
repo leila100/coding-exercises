285ec760a8805374431c50b42362f7f7fc9ee63a
Leila <nb.leila10@gmail.com>
2020-04-21 19:42:05 -0500

    Answer challenge 6

--- frontend/JS/async/async.js
+++ frontend/JS/async/async.js
@@ -29,9 +29,6 @@ a function func, a number interval, and another number duration.
 everyXsecsForYsecs will execute the given function every interval number of milliseconds, 
 but then automatically stop after duration milliseconds.
 
-Then pass the below sayHi function into an invocation of everyXsecsForYsecs with 1000 interval time an 5000 duration time.
-What do you expect to happen?
-
 Challenge 7
 Write a function delayCounter that accepts a number (called 'target') as the first argument and a number 
 of milliseconds (called 'wait') as the second argument, and returns a function.
@@ -133,18 +130,32 @@ function limitedRepeat() {
   var printEverySecond = setInterval(printMessage, 1000);
 }
 // Uncomment the following line to check your work!
-limitedRepeat(); // should log (every second, for 5 seconds): hi for now
+//limitedRepeat(); // should log (every second, for 5 seconds): hi for now
 
 /* CHALLENGE 6 */
+/*
+Write a function called everyXsecsForYsecs that will accept three arguments: 
+a function func, a number interval, and another number duration.
 
-function everyXsecsForYsecs() {
-  // ADD CODE HERE
+everyXsecsForYsecs will execute the given function every interval number of milliseconds, 
+but then automatically stop after duration milliseconds.
+
+*/
+
+function everyXsecsForYsecs(func, interval, duration) {
+  var counter = 0;
+  function printMessage() {
+    func();
+    counter += interval;
+    if (counter >= duration) clearInterval(printEverySecond);
+  }
+  var printEverySecond = setInterval(printMessage, interval);
 }
 // Uncomment the following lines to check your work!
-// function theEnd() {
-//   console.log('This is the end!');
-// }
-// everyXsecsForYsecs(theEnd, 2, 20); // should invoke theEnd function every 2 seconds, for 20 seconds): This is the end!
+function theEnd() {
+  console.log("This is the end!");
+}
+everyXsecsForYsecs(theEnd, 2000, 20000); // should invoke theEnd function every 2 seconds, for 20 seconds): This is the end!
 
 /* CHALLENGE 7 */
 
