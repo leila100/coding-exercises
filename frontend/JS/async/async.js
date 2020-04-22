641edbbe4e74c12d0136aaa9a9f80829dc6a1760
Leila <nb.leila10@gmail.com>
2020-04-21 20:19:10 -0500

    Answer challenge 9

--- frontend/JS/async/async.js
+++ frontend/JS/async/async.js
@@ -197,27 +197,63 @@ function promised(val) {
 }
 
 // UNCOMMENT THESE TO TEST YOUR WORK!
-const createPromise = promised("wait for it...");
-createPromise.then((val) => console.log(val));
+// const createPromise = promised("wait for it...");
+// createPromise.then((val) => console.log(val));
 // will log "wait for it..." to the console after 2 seconds
 
 /* CHALLENGE 9 */
+/*
+Write a SecondClock class, with two methods: start and reset.​
+start: upon invocation, invokes a callback (this.cb, defined in the constructor) on an argument every second, 
+with the argument to the callback being the current seconds "value".
+
+In other words, the callback gets invoked every second on the "seconds hand" of the clock. 
+Always start with 1 and don't utilize the seconds value of the current computer clock time.
+
+The first "tick" with value 1 occurs 1 second after the initial "secondClock" invocation.
+The second "tick" with value 2 occurs 2 seconds after the initial "secondClock" invocation.
+...
+The sixtieth "tick" with value 60 occurs 60 seconds after the initial "secondClock" invocation.
+The sixty-first "tick" with value 1 occurs 61 seconds after the initial "secondClock" invocation.
+The sixty-second "tick" with value 2 occurs 62 seconds after the initial "secondClock" invocation.
+etc.
+reset: upon invocation, completely stops the "clock".
+Also resets the time back to the beginning.
+​
+Hint: look up setInterval and clearInterval.
+*/
 
 class SecondClock {
   constructor(cb) {
-    // ADD CODE HERE
+    this.cb = cb;
+    this.secondsVal = 0;
+    this.stopInterval = null;
   }
-  // ADD METHODS HERE
+  printSeconds = () => {
+    this.secondsVal = this.secondsVal % 60;
+    this.secondsVal++;
+    this.cb(this.secondsVal);
+  };
+  start() {
+    this.stopInterval = setInterval(this.printSeconds, 1000);
+  }
+  reset() {
+    clearInterval(this.stopInterval);
+    this.secondsVal = 1;
+    this.stopInterval = null;
+  }
 }
 
 // UNCOMMENT THESE TO TEST YOUR WORK!
-// const clock = new SecondClock((val) => { console.log(val) });
-// console.log("Started Clock.");
-// clock.start();
-// setTimeout(() => {
-//     clock.reset();
-//     console.log("Stopped Clock after 6 seconds.");
-// }, 6000);
+const clock = new SecondClock((val) => {
+  console.log(val);
+});
+console.log("Started Clock.");
+clock.start();
+setTimeout(() => {
+  clock.reset();
+  console.log("Stopped Clock after 6 seconds.");
+}, 6000);
 
 /* CHALLENGE 10 */
 
