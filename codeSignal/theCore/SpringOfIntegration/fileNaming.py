e7657e98f2033ed64d12b56178db7a6c1b09cb71
Leila <nb.leila10@gmail.com>
2020-09-17 13:15:49 -0500

    Add solution to fileNaming problem

---
+++ codeSignal/theCore/SpringOfIntegration/fileNaming.py
@@ -0,0 +1,47 @@
+'''
+You are given an array of desired filenames in the order of their creation. 
+Since two files cannot have equal names, the one which comes later will have 
+an addition to its name in a form of (k), where k is the smallest positive 
+integer such that the obtained name is not used yet.
+
+Return an array of names that will be given to the files.
+
+Example
+
+For names = ["doc", "doc", "image", "doc(1)", "doc"], the output should be
+fileNaming(names) = ["doc", "doc(1)", "image", "doc(1)(1)", "doc(2)"].
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] array.string names
+
+Guaranteed constraints:
+5 ≤ names.length ≤ 1000,
+1 ≤ names[i].length ≤ 15.
+
+[output] array.string
+'''
+
+
+def fileNaming(names):
+    # keep track of file names and the current k for each file name
+    files = {}
+    solution = []
+    for name in names:
+        if name not in files:
+            files[name] = 0
+            solution.append(name)
+        else:
+            files[name] += 1    
+            newName = name + f"({files[name]})"
+            while newName in files:
+                files[name] += 1    
+                newName = name + f"({files[name]})"
+            solution.append(newName)
+            files[newName] = 0
+    return solution
+
+names = ["doc", "doc", "image", "doc(1)", "doc"]
+print(fileNaming(names))
